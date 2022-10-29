ab57f2d3c600b7998350f5f965eabc1e4ea64b22
Juanchristian87 <juan.422021016@civitas.ukrida.ac.id>
2022-10-29 16:12:33 +0700

    Test Slicing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('login', function () {
+    return view('auth.user.login');
+});
+Route::get('checkout/{camp}', function () {
+    return view('checkout.create');
+});
+Route::get('checkout/success', function () {
+    return view('checkout.success');
 });
